ab546761b23d16fec720f38b57972033ba30087b
Andrew Boltachev <andrewboltachev@gmail.com>
2015-11-28 16:39:38 +0300

    conform checkers

--- src/regexpforobj/main.clj
+++ src/regexpforobj/main.clj
@@ -1,5 +1,18 @@
-(ns regexpforobj.main)
+(ns regexpforobj.main
+  (:use regexpforobj.core)
+  )
+
+(require '[fipp.edn :refer (pprint) :rename {pprint fipp}])
+(def s1 (Seq [(Char "ref") (Star (Char "ref"))]))
+(defn structure-conforms-to [pattern s]
+
+  )
 
 (defn main []
-  (println "Hello, Peter!")
+  (fipp
+   #_(clojure.walk/prewalk (fn [x] (cond (and (map? x) (contains? x :type)) (assoc x :a 1) :else x)) s1)
+    (structure-conforms-to
+      (Seq ['x 'x])
+      (Seq [(Char "ref") (Char "ref")]))
+    )
   )
